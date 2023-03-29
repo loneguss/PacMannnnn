3ed943965a2d4a5bcd934d642ee3e13448e1de37
loneguss <pitcha.huta@bumail.net>
2023-03-29 17:12:13 +0700

    Add Relay

---
+++ Assets/Script/Relay.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using QFSW.QC;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using UnityEngine;
+
+public class Relay : NetworkBehaviour
+{
+    private async void Start()
+    {
+       await UnityServices.InitializeAsync();
+       await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+    
+    [Command]
+    private async void CreateRelay()
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(3);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            
+            Debug.Log("Join Code : " + joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+            
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartHost();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+            throw;
+        }
+    }
+    
+    [Command]
+    private async void JoinRelay(string joinCode)
+    {
+        try
+        {
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+            
+            RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+            
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            
+            NetworkManager.Singleton.StartClient();
+
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+            throw;
+        }
+    }
+}
