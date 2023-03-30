1eda1407162125e5923e33a27003da6950211073
loneguss <pitcha.huta@bumail.net>
2023-03-30 20:29:31 +0700

    add team color

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,30 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using QFSW.QC;
 using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
+    public enum Team
+    {
+        Red,
+        Blue,
+        White
+    }   
+    
+    
     [SerializeField] private GameObject playerBase;
     private bool isDead = false;
     private PlayerTeleport _playerTeleport;
     [SerializeField] private NetworkVariable<FixedString64Bytes> playerName = new NetworkVariable<FixedString64Bytes>
         ("player",NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
+
+    private Team _team = Team.White;
     
     // Start is called before the first frame update
     void Start()
     {
+        SetTeamServerRpc(_team);
+        
+        
         if(!IsOwner) return;
         playerName.Value = "Player:" + NetworkManager.Singleton.LocalClientId.ToString();
         _playerTeleport = GetComponent<PlayerTeleport>();
         playerBase = GameObject.FindWithTag("Base");
         GetComponent<Gun>().playerName = playerName.Value.ToString();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+
     }
 
     public void Dead()
@@ -43,5 +59,35 @@ public class Player : NetworkBehaviour
     private void SetPlayerNameServerRpc()
     {
         Debug.Log("fuickkk");
+    }
+
+
+    [ServerRpc(RequireOwnership = false)]
+    public void SetTeamServerRpc(Team _team)
+    {
+
+        this._team = _team;
+        
+        if (_team == Team.Red)
+        {
+            SetTeamClientRpc(Color.red,_team);
+        }
+        
+        if (_team == Team.White)
+        {
+            SetTeamClientRpc(Color.white,_team);
+        }
+
+        if (_team == Team.Blue)
+        {
+            SetTeamClientRpc(Color.blue,_team);
+        }
+    }
+    
+    [ClientRpc]
+    private void SetTeamClientRpc(Color color,Team _team)
+    {
+        this._team = _team;
+        GetComponent<SpriteRenderer>().color = color;
     }
 }
